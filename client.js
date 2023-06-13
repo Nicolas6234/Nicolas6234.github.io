370847cb1d4db92abc794a90c806e42fd86b639d
caroff <nicolas.caroff@insa-rennes.fr>
2023-06-13 21:22:23 +0200

    V1.0.6

--- client.js
+++ client.js
@@ -2,11 +2,11 @@
 import * as THREE from 'three'
 import {FBXLoader} from '/node_modules/three/examples/jsm/loaders/FBXLoader.js'
 import {GLTFLoader} from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'lil-gui'
+import * as dat from '/node_modules/lil-gui'
 import {FontLoader} from '/node_modules/three/examples/jsm/loaders/FontLoader.js'
 import {TextGeometry} from '/node_modules/three/examples/jsm/geometries/TextGeometry.js'
 import {PointerLockControls} from '/node_modules/three/examples/jsm/controls/PointerLockControls.js'
-import * as TWEEN from '@tweenjs/tween.js'
+import * as TWEEN from '/node_modules/@tweenjs/tween.js'
 var fence;
 var coords = { x: 5, y: 1, z: 25 };
 var coords2 = { x: 19, y: 1, z: 19 };
